47c97035b882bc722fe252f66e1b9abdba1ecb3c
Carlos Alexander <83734836+carlosalex96@users.noreply.github.com>
2024-02-02 22:51:14 -0300

    Update script.js
    acrescentadas vogais com acento

--- script.js
+++ script.js
@@ -14,16 +14,26 @@ function processText() {
 
 function criptografar(text) {
     return text.replace(/e/g, 'enter')
+                .replace(/é/g, 'enter')
                .replace(/i/g, 'imes')
+               .replace(/í/g, 'enter')
                .replace(/a/g, 'ai')
+               .replace(/á/g, 'enter')
                .replace(/o/g, 'ober')
-               .replace(/u/g, 'ufat');
+               .replace(/ó/g, 'enter')
+               .replace(/u/g, 'ufat')
+               .replace(/ú/g, 'enter');
 }
 
 function descriptografar(text) {
     return text.replace(/enter/g, 'e')
+                .replace(/enter/g, 'é')
                .replace(/imes/g, 'i')
+               .replace(/enter/g, 'í')
                .replace(/ai/g, 'a')
+               .replace(/enter/g, 'á')
                .replace(/ober/g, 'o')
-               .replace(/ufat/g, 'u');
+               .replace(/enter/g, 'ó')
+               .replace(/ufat/g, 'u')
+               .replace(/enter/g, 'ú');
 }
